ab08222df042a41fb37dd99fd51223b44c79ba3e
Manal Soava <manal.hibat.allah@gmail.com>
2022-10-12 10:47:16 +0200

    la mise en place de la page d'accueil

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,38 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const { id, name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    // image informative contient un alt
+    img.setAttribute("alt", "portrail de " + name);
+
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+
+    const a = document.createElement("a");
+    a.setAttribute("href", "photographer.html?id=" + id);
+    // Ajouter un titre au lien avec le nom du photographe
+    a.setAttribute("title", name);
+
+    const h3 = document.createElement("h3");
+    h3.textContent = city + ", " + country;
+    const p1 = document.createElement("p");
+    p1.textContent = tagline;
+    const p2 = document.createElement("p");
+    p2.classList.add("p_gray");
+    p2.textContent = price + "€/jour";
+    a.appendChild(img);
+    a.appendChild(h2);
+    article.appendChild(a);
+    article.appendChild(h3);
+    article.appendChild(p1);
+    article.appendChild(p2);
+    return article;
+  }
+  return { name, picture, city, getUserCardDOM };
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,14 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+const params = new URLSearchParams(document.location.search); // envoi les params du url
+const idParams = params.get("id"); // envoi le params en argument
+console.log(idParams);
+
+async function getPhotographer() {
+  const photographer = await fetch("../../data/photographers.json")
+    .then((res) => res.json())
+    .then((data) => data.photographers.find((p) => p.id == idParams));
+  console.log(photographer);
+  return photographer;
+}
+getPhotographer();
